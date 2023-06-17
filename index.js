975e531b8d860b7e51a6182a972eacd5ae703d94
SamuelWango <wangos478@gmail.com>
2023-06-17 12:18:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts)
+
+
+let thankYouLetter=["Guadalupe", "Ollie", "Aki"]
+let birthday=0
+let birthdayNotes=[]
+function writeCards(thankYouLetter,birthday){
+    for(birthday=0;birthday<thankYouLetter.length;birthday++){
+        birthdayNotes[birthday]=`Thank you, ${thankYouLetter[birthday]}, for the wonderful surprise gift!`
+        console.log(birthdayNotes[birthday])
+    }
+    return birthdayNotes
+}
+writeCards(thankYouLetter,birthday)
+
+
+let n
+function countDown(n){
+   while(n>=0){
+    console.log(n)
+    n--
+   } 
+   return
+}
+countDown(n)
